bb2fc1ebf645651331acf041b5327feb44e2d785
abdrea454 <emu024yesuf@gmail.com>
2023-09-13 00:00:24 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#replaces typo string in wp settings file
-exec { 'change to php':
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-setti\
-ngs.php",
-  path    => '/bin'
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,4 +0,0 @@
-# 0x17. Web stack debugging #3
-
-## Description
-What you should learn from this project:
